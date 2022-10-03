d813fe730adf992d3283190ee3141c57abff2539
Harrison Anthony <harrisoneanthony@gmail.com>
2022-10-02 21:42:55 -0400

    commit

--- pacman/script.js
+++ pacman/script.js
@@ -58,16 +58,16 @@ displayScore();
 // down = 40
 
 document.onkeydown = function(e){
-    if(e.keyCode == 37){
+    if(e.keyCode == 37 && world[pacman.y][pacman.x-1]!=2){
         pacman.x--;
     }
-    else if(e.keyCode == 39){
+    else if(e.keyCode == 39 && world[pacman.y][pacman.x+1]!=2){
         pacman.x++;
     }
-    else if (e.keyCode == 38){
+    else if (e.keyCode == 38 && world[pacman.y-1][pacman.x]!=2){
         pacman.y--;
     }
-    else if (e.keyCode == 40){
+    else if (e.keyCode == 40 && world[pacman.y+1][pacman.x]!=2){
         pacman.y++;
     }
     if(world[pacman.y][pacman.x] == 1){
